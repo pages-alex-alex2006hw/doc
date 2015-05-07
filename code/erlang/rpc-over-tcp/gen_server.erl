0ad123130ed6bf45edf5c70b3006fac4d0dd04f8
CJ <mv3142@gmail.com>
2015-05-07 00:45:06 -0700

    minor

--- code/erlang/rpc-over-tcp/gen_server.erl
+++ code/erlang/rpc-over-tcp/gen_server.erl
@@ -1,7 +1,18 @@
+%% module
 -module(mymod).
+
+%% behavior: generic server
 -behaviour(gen_server).
+
+%% API
+-export([]).
+
+%% gen_server callbacks
+%% - these callbacks will be executed
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, 
        terminate/2, code_change/3]).
+
+
 -record(state, {}).
 init([]) -> {ok, #state{}}.
 handle_call(_Request, _From, State) -> Reply = ok, {reply, Reply, State}.
