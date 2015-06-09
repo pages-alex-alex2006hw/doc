2f2e236463bd8cecd0df42a6e157ed83d1e503a5
CJ <mv3142@gmail.com>
2015-06-08 22:34:34 -0700

    update

--- code/lua/interp/main.c
+++ code/lua/interp/main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lua.h"
 #include "lauxlib.h"
+#include "lualib.h"
 
 int main(int argc, char **argv)
 {
-  lua_State *L = luaL_newstate();
-  if (L == NULL) {
+  int error;
+  
+  lua_State *L = NULL;
+  if ((L = luaL_newstate()) == NULL) {
     return EXIT_FAILURE;
   }
+  luaL_openlibs(L);
+  
 
-  char line[BUFSIZ];
-  while (fgets(line, sizeof(line), stdin) != 0)
-    luaL_dostring(L, line);
+  char line[256];
+  while (fgets(line, sizeof(line), stdin) != NULL) {
+    error = luaL_loadstring(L, line);
+    if (error)
+      fprintf(stderr, "%s\n", lua_tostring(L, -1));
+    if (error = lua_pcall(L, 0 /*nargs*/, 0/*nresults*/, 0/*errfunc*/) != 0) {
+      switch (error) {
+      case LUA_ERRRUN: printf("runtime error\n"); break;
+      case LUA_ERRMEM: printf("memory error\n"); break;
+      case LUA_ERRERR: printf("error during errhandler\n"); break;
+      default: printf("unknown error\n"); break;
+      }
+      lua_pop(L, 1);
+    }
+  }
+  lua_close(L);
   return 0;
 }
