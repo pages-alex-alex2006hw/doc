112a6fe98c8da3c75b7be36d864c7736a3228764
CJ <mv3142@gmail.com>
2015-06-23 22:53:37 -0700

    added pics

---
+++ etc/java.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/opt/glassfish3/jdk7
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=.
---
+++ etc/python.sh
@@ -0,0 +1,2 @@
+export PYTHONPATH=/home/cjeong/work/mv/src/build/tools/pyclient:$PYTHONPATH
+export PYTHONPATH=.:$PYTHONPATH
