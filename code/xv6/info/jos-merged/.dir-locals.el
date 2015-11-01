a6961cc2e276e8c548ece665d9005229eab2679c
MeadowView <mv3142@gmail.com>
2015-10-31 22:14:25 -0700

    minor

---
+++ code/xv6/info/jos-merged/.dir-locals.el
@@ -0,0 +1,12 @@
+((nil
+  (indent-tabs-mode . nil)
+  (tab-width . 2))
+ (c-mode
+  (c-file-style . "bsd")
+  (c-basic-offset . 2))
+ (shell-mode
+  (sh-basic-offset . 2)
+  (sh-indentation . 2))
+ (python-mode
+  (indent-tabs-mode . nil))
+ )
