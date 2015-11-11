d63ab0f2a726dc0bba50e76d50a451c5b8ecaf79
CJ <mv3142@gmail.com>
2015-11-10 22:56:02 -0800

    updated

--- code/lua/file-download/03-multi-thread.lua
+++ code/lua/file-download/03-multi-thread.lua
@@ -1,10 +1,13 @@
 local socket = require "socket"
 
 host = "www.w3.org"
-file = "/TR/REC-html32.html"
+file0 = "/TR/REC-html32.html"
+file1 = "/TR/html401/html40.txt"
+file2 = "/TR/2002/REC-xhtml1-20020801/xhtml1.pdf"
+file3 = "/TR/2000/REC-DOM-Level-2-Core-20001113/DOM2-Core.txt"
 
 function receive(connection)
-   connection:timeout(0) -- non-blocking
+   connection:settimeout(0) -- non-blocking
    local s, status, partial = connection:receive(2^10)
    if status == "timeout" then
       coroutine.yield(connection)
@@ -59,6 +62,7 @@ function dispatch()
 end
 
 
-
-
-download(host, file)
+get(host, file0)
+get(host, file1)
+get(host, file2)
+get(host, file3)
