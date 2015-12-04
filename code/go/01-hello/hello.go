c737193c7afbc1f4d926b03ba7a11ccc21cc29a7
CJ <mv3142@gmail.com>
2015-12-03 21:36:02 -0800

    updated

--- code/go/01-hello/hello.go
+++ code/go/01-hello/hello.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-  fmt.Println("Hello, World")
+	fmt.Println("Hello, World")
+	var s, sep string
+
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
